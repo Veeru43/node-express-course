6db4e7027756880511fc4ab2c0cfaac0b9a43097
VeerababuS <c_veerababu.s@infinitylearn.com>
2022-04-22 21:44:41 +0530

    done get api

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData=[
+    {name:'mark'},
+    {name:'jill'}
+]
+
+app.get('/users', function(req,res){
+    res.json({
+        success:true,
+        message: 'successfully got Users. Nice!',
+        users: mockUserData
+    })
+})
+
 app.listen(8000,function(){
     console.log("Server is Running")
 })
